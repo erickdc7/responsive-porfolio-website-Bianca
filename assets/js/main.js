1644fd1f9c745dcf20143f8bde47b4e2a279248b
Erick Díaz <erickdiazc717@gmail.com>
2026-08-10 14:28:03 -0500

    add scroll header function

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,14 @@ const typedHome = new Typed('#home-typed', {
 });
 
 /*=============== CHANGE HEADER STYLES ===============*/
-
+const scrollHeader = () => {
+   const header = document.getElementById('header')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+   this.scrollY >= 50
+      ? header.classList.add('scroll-header')
+      : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER WORK ===============*/
 
